4d6c58af6c391bdba3d08f1520aff7f2f40903a2
Brian Cavender <dercavendar@gmail.com>
2020-12-20 08:03:03 -0600

    randomized the min and max of the generated number to be guessed

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import math
 import random
 
 # set initial conditions with minimum and maximum values for the random number
-# TODO: randomize min_num and max_num to introduce variety
-min_num = 1
-max_num = 100
+min_num = random.randint(1, 10000)
+max_num = random.randint((min_num + 10), 10100)
 generated_number = random.randint(min_num, max_num)
 attempts = 1  # sets number of tries at 1 initially
 
 # provide initial hint for the random number either over or under half of max_num
+print(f'Guess a number between {min_num} and {max_num}')
 if (generated_number >= math.floor(max_num / 2)):
     print(f'the number is greater than or equal to {math.floor(max_num / 2)}')
 else:
