e6e201892e1359932b15f945ffc4958da5d16185
Brian Cavender <dercavendar@gmail.com>
2020-12-20 08:04:46 -0600

    added TODO for initial hint

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ attempts = 1  # sets number of tries at 1 initially
 
 # provide initial hint for the random number either over or under half of max_num
 print(f'Guess a number between {min_num} and {max_num}')
+# TODO: fix initial guess to be asscociated to the min and max not just max
 if (generated_number >= math.floor(max_num / 2)):
     print(f'the number is greater than or equal to {math.floor(max_num / 2)}')
 else:
