45d6352ca1682bd45e8966bb2b27a2742b32f2e5
Brian Cavender <dercavendar@gmail.com>
2020-12-20 07:48:09 -0600

    can now keep trying to guess with hint after each attempt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 min_num = 1
 max_num = 100
 generated_number = random.randint(min_num, max_num)
+attempts = 1
 
 if (generated_number >= math.floor(max_num / 2)):
     print(f'the number is greater than or equal to {math.floor(max_num / 2)}')
@@ -11,5 +12,9 @@ else:
     print(f'the number is less than {math.floor(max_num / 2)}')
 
 guess = int(input('what is your guess at the number? '))
-print(
-    f'The number was {generated_number} you were off by {abs(guess - generated_number)}')
+while (guess != generated_number):
+    print(
+        f'You were off by {abs(guess - generated_number)}, try again')
+    guess = int(input('what is your guess at the number? '))
+    attempts += 1
+print(f'You Got the Number! It took you {attempts} tries')
