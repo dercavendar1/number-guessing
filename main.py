297abea4ae2255f17f1c77344686b42c5fd86aa3
Brian Cavender <dercavendar@gmail.com>
2020-12-20 19:05:16 -0600

    cleaned up and added more clear documentation

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import random
 
 # set initial conditions with minimum and maximum values for the random number
 min_num = random.randint(1, 10000)
+# generates a random max number that will be at least 10 greater than min_num
 max_num = random.randint((min_num + 10), 10100)
+# generates the random number to be guessed
 generated_number = random.randint(min_num, max_num)
+# initialize guess variable to prevent infinite loop
 guess = 0
-attempts = 1  # sets number of tries at 1 initially
+# sets number of tries at 1 initially
+attempts = 1
 
 # provide initial hint for the random number either over or under half of max_num
 print(f'Guess a number between {min_num} and {max_num}')
+# print this if generated_number is greater than half way between the range
 if (generated_number >= math.floor((((max_num - min_num)) / 2) + min_num)):
     print(
         f'the number is between {math.floor((((max_num - min_num)) / 2) + min_num)} and {max_num}')
+# print this if generated_number is less than half way between the range
 else:
     print(
         f'the number is between {min_num} and {math.floor((((max_num - min_num)) / 2) + min_num)}')
